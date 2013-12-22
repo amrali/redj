99a33d0e2dd5feb819224ace1c037010101e7da4
Amr Ali <amr.ali.cc@gmail.com>
2013-12-22 15:08:34 +0200

    Add basic IRC client

--- redj/irc.py
+++ redj/irc.py
@@ -1,13 +1,42 @@
+import ssl
 import irc.client
 
-class Client(irc.client.SimpleIRCClient):
+class Client(object):
     """\
     An IRC client to handle the most basic functionality.
     """
 
-    def __init__(self, target):
-        super(Client, self).__init__()
-        self.target = target
+    def __init__(self):
+        self.ircobj = irc.client.IRC()
+        self.ircobj.add_global_handler('all_events', self._dispatcher, -10)
+
+    def connect(self, *args, **kwargs):
+        """\
+        Connect to an IRC server.
+
+        :param server: Server name.
+        :param port: Port number.
+        :param nickname: The nickname.
+        :param password: Password (optional).
+        :param username: The username (optional).
+        :param ircname: The realname (optional).
+        """
+        self._connect(*args, **kwargs)
+
+    def ssl_connect(self, *args, **kwargs):
+        """\
+        Connect to an IRC server over SSL.
+
+        :param server: Server name.
+        :param port: Port number.
+        :param nickname: The nickname.
+        :param password: Password (optional).
+        :param username: The username (optional).
+        :param ircname: The realname (optional).
+        """
+        ssl_factory = irc.connection.Factory(wrapper=ssl.wrap_socket)
+        kwargs['connect_factory'] = ssl_factory
+        self.connect(*args, **kwargs)
 
     def on_welcome(self, conn, event):
         """\
@@ -31,4 +60,25 @@ class Client(irc.client.SimpleIRCClient):
         What should I do on disconnect.
         """
         pass # TODO: Implement
+
+    def on_kick(self, conn, event):
+        """\
+        What should I do when kicked out of a channel.
+        """
+        pass # TODO: Implement
+
+    def _dispatcher(self, conn, event):
+        """\
+        Dispatch events to an event handler if one is available.
+        """
+        do_nothing = lambda c, e: None
+        method = getattr(self, "on_{}".format(event.type), do_nothing)
+        method(connection, event)
+
+    def _connect(self, *args, **kwargs):
+        """\
+        Create a new connection.
+        """
+        conn = self.ircobj.server()
+        conn.connect(*args, **kwargs)
 
